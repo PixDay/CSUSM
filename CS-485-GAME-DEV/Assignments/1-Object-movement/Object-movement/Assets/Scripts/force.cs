38d56b680361ac0c341e2bdfc357c9944874900b
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-05 16:13:51 +0200

    [ADD] - Add force script to compare the behavior and add smash bros melee song

--- CS-485-GAME-DEV/Assignments/1-Object-movement/Object-movement/Assets/Scripts/force.cs
+++ CS-485-GAME-DEV/Assignments/1-Object-movement/Object-movement/Assets/Scripts/force.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class force : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private Vector3 vector;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Horizontal")) {
+            vector.x = Input.GetAxis("Horizontal");
+        }
+        if (Input.GetButton("Vertical")) {
+            vector.z = Input.GetAxis("Vertical");
+        }
+        rb.AddForce(vector * speed * Time.deltaTime);
+        vector.x = 0.0f;
+        vector.z = 0.0f;
+        vector.y = 0.0f;
     }
 }
