f30939b1d3711dce3e4e411d819a4cc25fd1fd69
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-05 15:55:56 +0200

    [ADD] - Football texture and model, script fixed for movement and freeze rotation

--- CS-485-GAME-DEV/Assignments/1-Object-movement/Object-movement/Assets/Scripts/movement.cs
+++ CS-485-GAME-DEV/Assignments/1-Object-movement/Object-movement/Assets/Scripts/movement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private Vector3 vector;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Horizontal")) {
+            vector.x = Input.GetAxis("Horizontal");
+        }
+        if (Input.GetButton("Vertical")) {
+            vector.z = Input.GetAxis("Vertical");
+        }
+        transform.Translate(vector * speed * Time.deltaTime);
+        vector.x = 0.0f;
+        vector.z = 0.0f;
+        vector.y = 0.0f;
     }
 }
