4552e73a1ebf4b57594073dd8af1c332a0242f6f
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-20 17:27:43 +0200

    [ADD] - PlayerMovement script created and working

---
+++ CS-485-GAME-DEV/Assignments/3-Object-translation/Object-translation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Vector3 vector;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal")) {
+            vector.x = Input.GetAxis("Horizontal");
+        }
+        if (Input.GetButton("Vertical")) {
+            vector.z = Input.GetAxis("Vertical");
+        }
+        rigidbody.AddForce(vector * speed * Time.deltaTime);
+        vector.x = 0.0f;
+        vector.z = 0.0f;
+        vector.y = 0.0f;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "CollidedSphere")
+            Destroy(collision.collider.gameObject);
+    }
+}
