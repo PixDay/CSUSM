838a9db43c494e2fc9006add46c29684824a4a8e
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-20 18:07:39 +0200

    [ADD] - Create movement script

---
+++ CS-485-GAME-DEV/Assignments/3-Object-translation/Object-translation/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Vector3 vector;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vector.x = Random.Range(-20.0f, 20.0f);
+        vector.z = Random.Range(-20.0f, 20.0f);
+        vector.y = 0.0f;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(vector * Time.deltaTime);
+    }
+}
