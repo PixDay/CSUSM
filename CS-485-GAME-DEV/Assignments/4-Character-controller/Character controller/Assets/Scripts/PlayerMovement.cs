3bc75462ae40b4db67f01f3978a17e23539c47ca
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-26 22:50:12 +0200

    [ADD] - Script for player movement done

--- CS-485-GAME-DEV/Assignments/4-Character-controller/Character controller/Assets/Scripts/PlayerMovement.cs
+++ CS-485-GAME-DEV/Assignments/4-Character-controller/Character controller/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.0f;
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            Debug.Log(Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
